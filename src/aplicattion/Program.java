db1f31780c6bdcc2fbc0bd27467ec11f58e531d3
Jonathan <34173007+Joe5561@users.noreply.github.com>
2023-06-07 15:14:13 -0300

    Atualização, versão 1.1
    Foi feita melhorias no software, foi adicionado as seguintes opções, 1 para iniciar e 0 para sair do sistema.

--- src/aplicattion/Program.java
+++ src/aplicattion/Program.java
@@ -14,41 +14,57 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        List<Funcionario> list = new ArrayList<>();
 
-        System.out.print("ENTRE COM O NÚMERO DE FUNCIONÁRIOS: ");
-        int n = sc.nextInt();
+        do {
 
-        for (int i = 1; i <= n; i++) {
-            System.out.println("DADOS DO FUNCIONÁRIO #: " + i);
-            System.out.print("FUNCIONÁRIO TERCERIZADO(S/N): ");
-            char ch = sc.next().charAt(0);
-            System.out.print("NOME: ");
-            sc.nextLine();
-            String nome = sc.nextLine();
-            System.out.print("HORAS: ");
-            int horas = sc.nextInt();
-            System.out.print("VALOR POR HORA: ");
-            double valorPorHora = sc.nextDouble();
-            if (ch == 's' || ch == 'S') {
-                System.out.print("DIGITE O VALOR ADICIONAL: ");
-                double valorAdicional = sc.nextDouble();
-                Funcionario funcionario = new FuncionarioTerceiro(nome, horas, valorPorHora, valorAdicional);
-                list.add(funcionario);
-            } else if (ch == 'n' || ch == 'N') {
-                Funcionario funcionario = new Funcionario(nome, horas, valorPorHora);
-                list.add(funcionario);
-            }else {
+            System.out.print("BEM VINDO AO SISTEMA, DIGITE 1 PARA INICIAR OU 0 PARA SAIR: ");
+            int opStart = sc.nextInt();
+            if (opStart == 1) {
+
+                List<Funcionario> list = new ArrayList<>();
+                System.out.print("ENTRE COM O NÚMERO DE FUNCIONÁRIOS: ");
+                int n = sc.nextInt();
+
+                for (int i = 1; i <= n; i++) {
+                    System.out.println("DADOS DO FUNCIONÁRIO #: " + i);
+                    System.out.print("FUNCIONÁRIO TERCERIZADO(S/N): ");
+                    char ch = sc.next().charAt(0);
+                    System.out.print("NOME: ");
+                    sc.nextLine();
+                    String nome = sc.nextLine().toUpperCase();
+                    System.out.print("QUANTIDADE DE HORAS: ");
+                    int horas = sc.nextInt();
+                    System.out.print("VALOR POR HORA: ");
+                    double valorPorHora = sc.nextDouble();
+                    if (ch == 's' || ch == 'S') {
+                        System.out.print("DIGITE O VALOR ADICIONAL: ");
+                        double valorAdicional = sc.nextDouble();
+                        Funcionario funcionario = new FuncionarioTerceiro(nome, horas, valorPorHora, valorAdicional);
+                        list.add(funcionario);
+                    } else if (ch == 'n' || ch == 'N') {
+                        Funcionario funcionario = new Funcionario(nome, horas, valorPorHora);
+                        list.add(funcionario);
+                    } else {
+                        System.out.println();
+                        System.out.println("OPÇÃO INVÁLIDA");
+                    }
+                }
+
                 System.out.println();
-                System.out.println("OPÇÃO INVÁLIDA");
+                System.out.println("PAGAMENTOS: ");
+                for (Funcionario funcionario : list) {
+                    System.out.println(funcionario.getNome() +
+                            "- R$ " + String.format("%.2f", funcionario.pagamento()));
+                }
+                System.out.println();
+            } else if (opStart == 0) {
+                System.exit(0);
+                sc.close();
+            } else {
+                System.out.println();
+                System.out.print("OPÇÃO INVÁLIDA!!!");
+                System.out.println();
             }
-        }
-        System.out.println();
-        System.out.println("PAGAMENTOS: ");
-        for (Funcionario funcionario : list) {
-            System.out.println(funcionario.getNome() + "- $" + String.format("%.2f", funcionario.pagamento()));
-        }
-        sc.close();
-
+        } while (true);
     }
 }
